124a1404fb30c77896f66bac16c9479e99076e0a
Dauka12 <93545836+Dauka12@users.noreply.github.com>
2023-03-05 00:03:29 +0600

    first commit

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'summary': 'Hospital Management system',
+    'description': """Hospital Management system""",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': False,
+    'auto_install': False,
+    'assets': {},
+    'license':'LGPL-3',
+
+
+}
